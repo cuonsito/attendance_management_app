a767e702920cfeb6e30f97da5bb523da33553038
sito <sito@cuon.co.jp>
2015-04-28 14:09:05 +0900

    userモデルのvalidation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :validatable
+	validates :name, presence: true, length: { maximum: 20 }
+	VALID_EMAIL_REGEX = /\A[-+.\w]+@(?:[-\w]+\.)+\[:alpha:]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 }
 end
