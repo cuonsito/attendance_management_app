4a2dcda1644e0401777781a0f851bb48b8339cd8
sito <sito@cuon.co.jp>
2015-04-28 15:25:28 +0900

    userモデルのvalidation、テスト

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,6 @@
 class Member < ActiveRecord::Base
+	validates :user_id,     presence: true
+  validates :project_id,  presence: true
+  validates :assain_rate, presence: true, numericality: { greater_than_or_equal_to: 0,
+  																											  less_than_or_equal_to: 1 }
 end
