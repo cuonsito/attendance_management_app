f4983f9a116739009cd800891c776f8250aa0b05
sito <sito@cuon.co.jp>
2015-04-30 17:51:21 +0900

    sign inページへのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :sessions, only: [:new, :delete]
   root 'static_pages#home'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 end
