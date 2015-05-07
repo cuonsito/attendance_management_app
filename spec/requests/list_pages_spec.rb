629302bb94f600456cb6e90f1e4659060774920f
sito <sito@cuon.co.jp>
2015-05-07 10:29:52 +0900

    listページのテスト

--- spec/requests/list_pages_spec.rb
+++ spec/requests/list_pages_spec.rb
@@ -4,15 +4,15 @@ describe "List Pages" do
 
   subject { page }
 
-  # describe "List page" do
-  #   let(:user) { FactoryGirl.create(:user) }
-  #   before do
-  #     sign_in user
-  #     visit list_path
-  #   end
+  describe "List page", type: :request do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      login user
+      visit list_path
+    end
 
-  #   it { should have_content('List') }
-  #   it { should have_title("Attendance Management App") }
-  #   it { should have_title('| List') }
-  # end
+    it { should have_content('社員一覧') }
+    it { should have_title("Attendance Management App") }
+    it { should have_title('| List') }
+  end
 end
