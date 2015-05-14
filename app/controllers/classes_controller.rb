c38e4816bf97bcfd7bcf777d8811d67cfb87cc31
sito <sito@cuon.co.jp>
2015-05-14 21:59:28 +0900

    ルーティングの変更

---
+++ app/controllers/classes_controller.rb
@@ -0,0 +1,8 @@
+class ClassesController < ApplicationController
+
+  before_action :authenticate_user!, only: :index
+
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :delete]
   root 'static_pages#home'
   match '/menu',     to: 'static_pages#menu',     via: 'get'
-  match '/classes',  to: 'static_pages#classes',  via: 'get'
   
   resources :users, only: [:index], shallow: true do 
     resources :working_times, only: [:index, :create, :destroy]
@@ -12,5 +11,6 @@ Rails.application.routes.draw do
   patch 'user/:user_id/projects/:project_id/working_times/approve_all', to: 'working_times#approve_all', as: :approve_all
 
   match '/projects', to: 'projects#index', via: 'get'
+  match '/classes',  to: 'classes#index',  via: 'get'
 
 end
